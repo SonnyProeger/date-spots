cf0aa0e4240cc5e350e984bd384d6a8beaede855
sonny <sonnyproeger@outlook.com>
2023-10-08 14:24:59 +0200

    Added barebones for hero and homepage

---
+++ resources/js/Components/Hero.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+name: "Hero"
+}
+</script>
+
+<!-- Hero.vue -->
+<template>
+  <div class="bg-blue-500 py-16">
+    <div class="container mx-auto text-center text-white">
+      <h1 class="text-4xl font-bold mb-4">Welcome to our website</h1>
+      <p class="text-lg">Discover amazing content and more!</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,35 @@
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
+import Hero from "@/Components/Hero.vue";
+
+export default {
+    name: "Home",
+    components: {Hero, Welcome, AppLayout}
+}
+</script>
+
+<template>
+    <AppLayout title="Home">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+
+        <hero>
+
+        </hero>
+<!--        <div class="py-12">-->
+<!--            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">-->
+<!--                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">-->
+<!--                    <Welcome />-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
