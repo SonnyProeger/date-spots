380eaf892fe63d29b45d910de13042a02a305812
sonny <sonnyproeger@outlook.com>
2023-10-11 13:56:07 +0200

    made / redirect to home

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,12 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('home');
 });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
-Route::get('/home', function () {
-    return Inertia::render('Home');
-});
 
 Route::middleware([
     'auth:sanctum',
