ffaeae25d23cc45cccc66c05e205ec82fb9d929a
sonny <sonnyproeger@outlook.com>
2023-10-13 16:18:03 +0200

    Refactored how it works, make the cards scaleable and reusable components

--- resources/js/Components/HowItWorks.vue
+++ resources/js/Components/HowItWorks.vue
@@ -1,6 +1,17 @@
 <script>
+import StepCard from "@/Components/StepCard.vue";
+
 export default {
-    name: "HowItWorks"
+    name: "HowItWorks",
+    components: {StepCard},
+    methods: {
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth", // Add smooth scrolling behavior
+            });
+        },
+    },
 }
 </script>
 
@@ -8,44 +19,42 @@ export default {
     <div class="py-12 bg-white">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 class="text-3xl font-extrabold text-gray-900 text-center">How It Works</h2>
-            <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3">
-                <!-- Step 1: Enter Location -->
-                <div class="group relative">
-                    <div class="rounded-lg overflow-hidden bg-roseGold group-hover:bg-rose-700 p-4">
-                        <!-- Step 1 Icon/Image -->
-                        <img src="../../../public/images/location.png" alt="Location Icon" class="w-16 h-16 mx-auto" />
-                        <!-- Step 1 Description -->
-                        <p class="mt-2 text-lg font-medium text-white">1. Enter Location</p>
-                        <p class="mt-1 text-black">Tell us where you would like to go for your date night.</p>
-                    </div>
-                </div>
+            <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-4">
+                <StepCard
+                step-title="1. Enter Location"
+                step-description="Tell us where you would like to go for your date night."
+                icon-src="/images/location.png"
+                alt-text="Location Icon"
+                >
 
-                <!-- Step 2: Find Date Spot -->
-                <div class="group relative">
-                    <div class="rounded-lg overflow-hidden bg-roseGold group-hover:bg-rose-700 p-4">
-                        <!-- Step 2 Icon/Image -->
-                        <img src="../../../public/images/answer.png" alt="Search Icon" class="w-16 h-16 mx-auto" />
-                        <!-- Step 2 Description -->
-                        <p class="mt-2 text-lg font-medium text-white">2. Find Date Spot</p>
-                        <p class="mt-1 text-black">Browse our amazing date spot recommendations.</p>
-                    </div>
-                </div>
+                </StepCard>
 
-                <!-- Step 3: Go on a Date -->
-                <div class="group relative">
-                    <div class="rounded-lg overflow-hidden bg-roseGold group-hover:bg-rose-700 p-4">
-                        <!-- Step 3 Icon/Image -->
-                        <img src="../../../public/images/dating.png" alt="Date Icon" class="w-16 h-16 mx-auto" />
-                        <!-- Step 3 Description -->
-                        <p class="mt-2 text-lg font-medium text-white">3. Go on a Date</p>
-                        <p class="mt-1 text-black">Plan your perfect date night and make lasting memories.</p>
-                    </div>
-                </div>
+                <StepCard
+                    step-title="2. Find Date Spot"
+                    step-description="Browse our amazing date spot recommendations!"
+                    icon-src="/images/answer.png"
+                    alt-text="Search Icon"
+                >
+
+                </StepCard>
+                <StepCard
+                    step-title="3. Go on a Date"
+                    step-description="Plan your perfect date night, make lasting memories."
+                    icon-src="/images/dating.png"
+                    alt-text="D Iatecon"
+                >
+                </StepCard>
+                <StepCard
+                    stepTitle="4. Leave a Review"
+                    stepDescription="Share your experience and help others discover great date spots."
+                    iconSrc="/images/review.png"
+                    altText="Review Icon"
+                ></StepCard>
             </div>
         </div>
         <div class="text-center mt-8">
             <!-- CTA Button -->
-            <a href="#" class="text-white bg-roseGold hover:bg-rose-700 px-6 py-3 rounded-md text-lg font-medium transition duration-300 ease-in-out">
+            <a @click="scrollToTop" class="text-white bg-roseGold hover:bg-rose-700 px-6 py-3 rounded-md text-lg font-medium transition duration-300 ease-in-out">
                 Get Started
             </a>
         </div>
