31f14279bd6af18a38a0a46baf9e8627dde1a1f8
sonny <sonnyproeger@outlook.com>
2023-11-06 23:40:30 +0100

    Added a (empty) detail for datespots

---
+++ resources/js/Pages/DateSpotDetail.vue
@@ -0,0 +1,18 @@
+<script>
+import NewAppLayout from "@/Layouts/newAppLayout.vue";
+
+export default {
+name: "DateSpotDetail",
+  components: {NewAppLayout}
+}
+</script>
+
+<template>
+  <NewAppLayout>
+
+  </NewAppLayout>
+</template>
+
+<style scoped>
+
+</style>
