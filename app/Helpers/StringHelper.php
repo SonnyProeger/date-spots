ea2df55efd7ee8ab83dee6e377d18a2bc9c34eb8
sonny <sonnyproeger@outlook.com>
2023-11-08 07:48:12 +0100

    Added Stringhelper class to remove hyphens from url

---
+++ app/Helpers/StringHelper.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Helpers;
+
+class StringHelper
+{
+    public static function replaceHyphensWithSpaces($string): array|string
+    {
+        // Use str_replace to replace hyphens with spaces
+        return str_replace('-', ' ', $string);
+    }
+}
