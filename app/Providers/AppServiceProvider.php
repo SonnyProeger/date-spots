272efe1497809fec5edd05402cdc146a94e8ae0f
sonny <sonnyproeger@outlook.com>
2023-11-10 16:08:40 +0100

    added google api env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+		//
+		Inertia::share('GOOGLE_API_KEY', config('app.google_api_key'));
+// I'm using config, but your could use env
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		//
+	}
 }
