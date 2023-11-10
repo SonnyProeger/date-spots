8a2e4fd682f02b15738e35972556175ec8108030
sonny <sonnyproeger@outlook.com>
2023-11-10 16:14:04 +0100

    Widen search bar for mobile

--- resources/js/Components/AddressForm.vue
+++ resources/js/Components/AddressForm.vue
@@ -1,50 +1,49 @@
 <script>
 export default {
-  name: "AddressForm",
-  mounted() {
-    let autocomplete;
-    let location;
-    let addressField = document.getElementById("addressSearchBar");
-    const options = {
-      types: ['(cities)'],
-      componentRestrictions: {
-            country: ['nl']
-          }
-    };
+	name: "AddressForm",
+	mounted() {
+		let autocomplete;
+		let location;
+		let addressField = document.getElementById("addressSearchBar");
+		const options = {
+			types: ['(cities)'],
+			componentRestrictions: {
+				country: ['nl']
+			}
+		};
 
-    autocomplete = new google.maps.places.Autocomplete(
-      addressField,
-      options,
-    );
+		autocomplete = new google.maps.places.Autocomplete(
+				addressField,
+				options,
+		);
 
-    autocomplete.addListener("place_changed", () => {
-        const selectedPlace = autocomplete.getPlace();
-        if (selectedPlace.name){
-            location = selectedPlace.name
-            this.$emit("address-updated", location)
-        }
-        else {
-          console.error("Invalid geometry information for the selected place:", selectedPlace);
-        }
-      // if (selectedPlace.geometry){
-      //   location = [selectedPlace.geometry.location.lat(),selectedPlace.geometry.location.lng()]
-      //   this.$emit("address-updated", location)
-      // }
-      // else {
-      //   console.error("Invalid geometry information for the selected place:", selectedPlace);
-      // }
-    });
-  },
+		autocomplete.addListener("place_changed", () => {
+			const selectedPlace = autocomplete.getPlace();
+			if (selectedPlace.name) {
+				location = selectedPlace.name
+				this.$emit("address-updated", location)
+			} else {
+				console.error("Invalid geometry information for the selected place:", selectedPlace);
+			}
+			// if (selectedPlace.geometry){
+			//   location = [selectedPlace.geometry.location.lat(),selectedPlace.geometry.location.lng()]
+			//   this.$emit("address-updated", location)
+			// }
+			// else {
+			//   console.error("Invalid geometry information for the selected place:", selectedPlace);
+			// }
+		});
+	},
 }
 </script>
 
 <template>
-  <input
-      id="addressSearchBar"
-      type="text"
-      class="w-1/2 px-4 py-2 border rounded-md mr-4 focus:outline-none"
-      placeholder="Enter your location (city)"
-  />
+	<input
+			id="addressSearchBar"
+			type="text"
+			class="w-2/3 md:w-1/2 px-4 py-2 border rounded-md mr-4 focus:outline-none"
+			placeholder="Enter your location (city)"
+	/>
 </template>
 
 <style scoped>
