d934eac5cb638a3452b2b4b6285c1bdb3e0cec96
sonny <sonnyproeger@outlook.com>
2023-11-13 11:36:13 +0100

    Added mixindue to bloat, final lay-out version of filter

--- resources/js/Components/FilterComponents/DateSpotCityFilter.vue
+++ resources/js/Components/FilterComponents/DateSpotCityFilter.vue
@@ -1,116 +1,41 @@
 <script>
+import {dateSpotCityFilterMixin} from "@/mixins/dateSpotCityFilterMixin.js";
+
 export default {
 	name: "DateSpotCityFilter",
 	emits: ['saveFilter'],
+	mixins: [dateSpotCityFilterMixin],
 	data() {
 		return {
+			isLargeScreen: window.innerWidth >= 768,
 			isFilterVisible: false,
 			selectedTypes: [],
 			selectedCategories: [],
 			selectedSubCategories: [],
-			showCategories: false,
-			showTypes: false,
+			showCategories: [],
+			showSubCategories: [],
 		}
 	},
+
 	props: {
 		types: Object,
 		categories: Object,
 		subCategories: Object,
 	},
+
 	methods: {
-		selectAllSubcategories(category) {
-			// Check if all subcategories within the given category are already selected
-			if (this.isCategorySelected(category)) {
-				// If not all are selected, select all subcategories by adding to the selectedSubCategories array
-				// Filter te ones that are already added, so no duplicates
-
-				const newSubcategories = category.subCategories
-						.map(subcategory => `${category.id}-${subcategory.id}`)
-						.filter(subcategory => !this.selectedSubCategories.includes(subcategory));
-
-				this.selectedSubCategories = [...this.selectedSubCategories, ...newSubcategories];
-			} else {
-				// If all are selected, deselect them by removing from the selectedSubCategories array
-				this.selectedSubCategories = this.selectedSubCategories.filter(value => {
-					return !value.startsWith(`${category.id}-`);
-				});
-			}
-		},
-
-		checkIfAllSubCategoriesSelected(category, subCategory) {
-			const categoryId = category.id;
-
-			// Check if the Category is currently selected, and the specific subcategory is being deselected
-			if (
-					this.selectedCategories.includes(categoryId) &&
-					!this.selectedSubCategories.includes(subCategory)
-			) {
-				// Deselect the category
-				this.selectedCategories = this.selectedCategories.filter(value => value !== categoryId);
-			}
-
-			// Generate identifiers for all subcategories within the category
-			const subcategoriesForCategory = category.subCategories.map(subCategory => `${category.id}-${subCategory.id}`);
-
-			// Check if all subcategories within the category are selected
-			if (subcategoriesForCategory.every(subCategory =>
-					this.selectedSubCategories.includes(subCategory)
-			)) {
-				// If all subCategories are selected and the Category is not already selected, select the Category
-				if (!this.selectedCategories.includes(categoryId)) {
-					this.selectedCategories = [
-						...this.selectedCategories,
-						categoryId
-					];
-				}
-			}
-		},
-
-		isCategorySelected(category) {
-			// Check if "Select All" is selected for a category
-			return this.selectedCategories.includes(category.id);
-		},
-
-		toggleFilter() {
-			// Toggle the visibility of the filter
-			this.isFilterVisible = !this.isFilterVisible;
-		},
-
-		toggleFilterDropdownMobile(dropdownType) {
-			if (dropdownType === 'type') {
-				this.showTypes = !this.showTypes;
-			} else if (dropdownType === 'category') {
-				this.showCategories = !this.showCategories;
-			}
-		},
-
 		saveAndCloseFilter() {
 			this.$emit('saveFilter');
 			this.toggleFilter();
 		},
-
 	},
-
 }
 
 </script>
 
 <template>
-	<!-- Filter Desktop -->
-	<div class="hidden md:block w-1/4 p-4 bg-gray-100">
-
-		<!-- Settings options -->
-		<h2 class="text-lg font-semibold mb-4">Settings</h2>
-
-		<!-- Example settings -->
-		<label for="setting-1">Setting 1</label>
-		<!--				<input id="setting-1" type="checkbox" v-model="setting1">-->
-		<!-- Add more settings as needed -->
-
-	</div>
-
 	<!--Filter Mobile-->
-	<div class="block">
+	<div class="block md:hidden">
 		<!-- Filter Button -->
 		<button @click="toggleFilter"
 		        class="w-full bg-roseGold text-white px-4 py-2 rounded flex items-center justify-center">
@@ -124,102 +49,87 @@ export default {
 	</div>
 
 	<!-- Filter Content (Hidden by default) -->
-	<div>
-		<!-- Filter overlay -->
-		<div v-show="isFilterVisible" class="absolute top-0 left-0 h-full w-full bg-white flex flex-col space-y-12">
-			<div class="absolute top-2 right-2 p-2" @click="toggleFilter">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-				     stroke="currentColor" class="w-6 h-6">
-					<path stroke-linecap="round" stroke-linejoin="round"
-					      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
-				</svg>
-			</div>
+	<!-- Filter overlay -->
+	<div v-show="isFilterVisible || isLargeScreen"
+	     class="md:py-4 md:w-1/4 md:rounded-t-lg md:object-cover md:h-full md:block md:relative flex flex-col absolute h-screen w-screen"
+	>
 
-			<div class="bg-white rounded">
-				<p class="pl-4 text-lg font-bold">Filters</p>
+		<div class="bg-white rounded-lg shadow-md h-full w-full">
+			<p class="pl-4 pt-4 text-lg font-bold">Filters</p>
+			<div class="px-6">
 				<hr class="w-full m-2">
-				<div class="px-6">
-					<div class="flex justify-between" @click="toggleFilterDropdownMobile('type')">
-						<p class="text-md font-semibold">Types</p>
-						<svg v-if="showTypes" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-						     stroke-width="1.5"
-						     stroke="currentColor" class="w-6 h-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
-						</svg>
-						<svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-						     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
-						</svg>
-					</div>
 
-					<hr class="w-full m-2">
-
-					<!-- Types filter options -->
-					<div v-show="showTypes" v-for="type in types" :key="type.id">
-						<input type="checkbox" v-model="selectedTypes" :value="type.id">{{ type.name }}
-					</div>
-
-					<div class="flex justify-between" @click="toggleFilterDropdownMobile('category')">
-						<p class="text-md font-semibold">Categories</p>
-						<svg v-if="showCategories" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-						     stroke-width="1.5"
-						     stroke="currentColor" class="w-6 h-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
-						</svg>
-						<svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						     stroke="currentColor" class="w-6 h-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
-						</svg>
-
-
-					</div>
-					<hr class="w-full m-2">
-
-					<!-- Categories filter options -->
-					<div v-show="showCategories" v-for="category in categories" :key="category.id">
-						<div v-if="category.subCategories && category.subCategories.length > 0">
-							<!-- If the category has subcategories, show a nested structure -->
-							<div>
-								<input
-										type="checkbox"
-										v-model="selectedCategories"
-										:value="category.id"
-										@change="selectAllSubcategories(category)"
-								/>
-								{{ category.name }}
-							</div>
-							<div class="pl-4">
-								<!-- Subcategories filter options -->
-								<div v-for="subCategory in category.subCategories" :key="subCategory.id">
-									<input
-											type="checkbox"
-											v-model="selectedSubCategories"
-											:value="`${category.id}-${subCategory.id}`"
-											@change="checkIfAllSubCategoriesSelected(category, subCategory)"
-									/>
-									{{ subCategory.name }}
-								</div>
-							</div>
+				<!--Type filter options-->
+				<div class="pb-4" v-for="type in types" :key="type.id">
+					<div class=" flex flex-row items-center">
+						<input type="checkbox" v-model="selectedTypes" :value="type.id" @change="selectAllCategories(type)">
+						<div class="flex justify-between w-full" @click="showCategoriesInFilter(type.id)">
+							<p class="pl-2 text-md font-semibold">{{ type.name }}</p>
+							<svg v-if="isShowCategorySelected(type.id)" xmlns="http://www.w3.org/2000/svg" fill="none"
+							     viewBox="0 0 24 24"
+							     stroke-width="1.5"
+							     stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
+							</svg>
+							<svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+							     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+							</svg>
 						</div>
+					</div>
+					<hr v-if="isShowCategorySelected(type.id)" class="w-full m-1">
 
-						<div v-else>
-							<!-- If there are no subcategories, display a regular checkbox -->
+					<!-- Category filter options -->
+					<div class="pl-4" v-if="isShowCategorySelected(type.id)" v-for="category in type.categories"
+					     :key="category.id">
+
+						<div class="py-1 flex flex-row items-center">
 							<input
 									type="checkbox"
 									v-model="selectedCategories"
-									:value="category.id"
+									:value="`${type.id}-${category.id}`"
+									@change="selectAllSubcategories(type, category)"
 							/>
-							{{ category.name }}
+							<div class="flex justify-between w-full" @click="showSubCategoriesInFilter(category.id)">
+								<p class="pl-2 text-md font-semibold">{{ category.name }}</p>
+								<svg v-if="isShowSubCategorySelected(category.id)" xmlns="http://www.w3.org/2000/svg" fill="none"
+								     viewBox="0 0 24 24"
+								     stroke-width="1.5"
+								     stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5"/>
+								</svg>
+								<svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+								     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+								</svg>
+							</div>
+						</div>
+						<hr v-if="isCategorySelected(category) || isShowSubCategorySelected(category.id)" class="w-full m-1">
+
+						<!--subcategory options-->
+						<div class="pl-4" v-if="isCategorySelected(category) || isShowSubCategorySelected(category.id)"
+						     v-for="subCategory in category.subCategories"
+						     :key="subCategory.id">
+							<div class="py-1 flex flex-row items-center">
+								<input
+										type="checkbox"
+										v-model="selectedSubCategories"
+										:value="`${category.id}-${subCategory.id}`"
+										@change="checkIfAllSubCategoriesSelected(type, category, subCategory)"
+								/>
+
+								<div class="flex justify-between w-full">
+									<p class="pl-2 text-md">{{ subCategory.name }}</p>
+								</div>
+							</div>
 						</div>
 					</div>
+				</div>
 
-					<!-- Add more filter options as needed -->
-
-					<!-- Save and Close buttons -->
-					<div class="mt-4 flex justify-center">
-						<button @click="saveAndCloseFilter" class="w-full bg-roseGold text-white px-3 py-1 rounded">Filter DateSpots
-						</button>
-					</div>
+				<!-- Save and Close button -->
+				<div class="p-4 md:pb-4 md:pt-2 flex justify-center">
+					<button @click="saveAndCloseFilter" class="w-full bg-roseGold text-white px-3 py-1 rounded">Filter DateSpots
+					</button>
 				</div>
 			</div>
 		</div>
---
+++ resources/js/mixins/dateSpotCityFilterMixin.js
@@ -0,0 +1,155 @@
+export const dateSpotCityFilterMixin = {
+    methods: {
+        selectAllCategories(type) {
+            // Check if all categories within the given type are already selected
+            if (this.isTypeSelected(type)) {
+                // If not all are selected, select all categories by adding to the selectedCategories array
+                // Filter the ones that are already added, so no duplicates
+
+                const newCategories = type.categories
+                    .map(category => `${type.id}-${category.id}`)
+                    .filter(category => !this.selectedCategories.includes(category));
+
+                this.selectedCategories = [...this.selectedCategories, ...newCategories];
+
+            } else {
+                // If all are selected, deselect them by removing from the selectedCategories array
+                this.selectedCategories = this.selectedCategories.filter(value => {
+                    return !value.startsWith(`${type.id}-`);
+                });
+            }
+            type.categories.forEach(category => {
+                // Trigger the selectAllSubcategories method for each category
+                this.selectAllSubcategories(type, category);
+            });
+        },
+
+        selectAllSubcategories(type, category) {
+            const categoryId = `${type.id}-${category.id}`
+            // Check if all subcategories within the given category are already selected
+            if (this.isCategorySelected(categoryId)) {
+                // If not all are selected, select all subcategories by adding to the selectedSubCategories array
+                // Filter the ones that are already added, so no duplicates
+
+                const newSubcategories = category.subCategories
+                    .map(subcategory => `${category.id}-${subcategory.id}`)
+                    .filter(subcategory => !this.selectedSubCategories.includes(subcategory));
+
+                this.selectedSubCategories = [...this.selectedSubCategories, ...newSubcategories];
+            } else {
+                // If all are selected, deselect them by removing from the selectedSubCategories array
+                this.selectedSubCategories = this.selectedSubCategories.filter(value => {
+                    return !value.startsWith(`${category.id}-`);
+                });
+            }
+            this.checkIfAllCategoriesSelected(type, category);
+        },
+
+        checkIfAllCategoriesSelected(type, category) {
+            const typeId = type.id;
+
+            // Check if the Category is currently selected, and the specific subcategory is being deselected
+            if (
+                this.selectedTypes.includes(typeId) &&
+                !this.selectedCategories.includes(category)
+            ) {
+                // Deselect the category
+                this.selectedTypes = this.selectedTypes.filter(value => value !== typeId);
+            }
+
+            // Generate identifiers for all subcategories within the category
+            const categoriesForType = type.categories.map(category => `${type.id}-${category.id}`);
+
+            // Check if all subcategories within the category are selected
+            if (categoriesForType.every(category =>
+                this.selectedCategories.includes(category)
+            )) {
+                // If all subCategories are selected and the Category is not already selected, select the Category
+                if (!this.selectedTypes.includes(typeId)) {
+                    this.selectedTypes = [
+                        ...this.selectedTypes,
+                        typeId
+                    ];
+                }
+            }
+        },
+
+        checkIfAllSubCategoriesSelected(type, category, subCategory) {
+            const categoryId = `${type.id}-${category.id}`;
+
+            // Check if the Category is currently selected, and the specific subcategory is being deselected
+            if (
+                this.selectedCategories.includes(categoryId) &&
+                !this.selectedSubCategories.includes(subCategory)
+            ) {
+                // Deselect the category
+                this.selectedCategories = this.selectedCategories.filter(value => value !== categoryId);
+            }
+
+            // Generate identifiers for all subcategories within the category
+            const subcategoriesForCategory = category.subCategories.map(subCategory => `${category.id}-${subCategory.id}`);
+
+            // Check if all subcategories within the category are selected
+            if (subcategoriesForCategory.every(subCategory =>
+                this.selectedSubCategories.includes(subCategory)
+            )) {
+                // If all subCategories are selected and the Category is not already selected, select the Category
+                if (!this.selectedCategories.includes(categoryId)) {
+                    this.selectedCategories = [
+                        ...this.selectedCategories,
+                        categoryId
+                    ];
+                }
+            }
+
+            this.checkIfAllCategoriesSelected(type, category)
+        },
+
+        showCategoriesInFilter(typeId) {
+            const index = this.showCategories.indexOf(typeId);
+
+            if (index !== -1) {
+                // If the clicked type is already visible, hide it
+                this.showCategories.splice(index, 1);
+            } else {
+                // If a different type is clicked, show its categories
+                this.showCategories.push(typeId);
+            }
+        },
+
+        showSubCategoriesInFilter(CategoryId) {
+            const index = this.showSubCategories.indexOf(CategoryId);
+
+            if (index !== -1) {
+                // If the clicked type is already visible, hide it
+                this.showSubCategories.splice(index, 1);
+            } else {
+                // If a different type is clicked, show its categories
+                this.showSubCategories.push(CategoryId);
+            }
+        },
+
+        isCategorySelected(categoryId) {
+            // Check if "Select All" is selected for a category
+            return this.selectedCategories.includes(categoryId);
+        },
+
+        isTypeSelected(type) {
+            // Check if "Select All" is selected for a category
+            return this.selectedTypes.includes(type.id);
+        },
+
+        isShowCategorySelected(typeId) {
+            return this.showCategories.includes(typeId);
+        },
+
+        isShowSubCategorySelected(categoryId) {
+            return this.showSubCategories.includes(categoryId);
+        },
+
+        toggleFilter() {
+            // Toggle the visibility of the filter
+            this.isFilterVisible = !this.isFilterVisible;
+        },
+    },
+};
