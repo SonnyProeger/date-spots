6c3a9129c72329e9efabd980d3144fb45165673d
sonny <sonnyproeger@outlook.com>
2023-11-13 14:22:51 +0100

    added failsafe for if there are no datespots

--- resources/js/Pages/DateSpotsCity.vue
+++ resources/js/Pages/DateSpotsCity.vue
@@ -2,37 +2,37 @@
 import NewAppLayout from "@/Layouts/newAppLayout.vue";
 import DateSpotCard from "@/Components/DateSpotCard.vue";
 import DateSpotCityFilter from "@/Components/FilterComponents/DateSpotCityFilter.vue";
+import {router} from '@inertiajs/vue3'
+
 
 export default {
 	name: "DateSpotsCity",
 	components: {DateSpotCityFilter, DateSpotCard, NewAppLayout},
-	created() {
-		console.log(this.dateSpots)
-	},
-	data() {
-		return {
-			isFilterVisible: false,
-			selectedTypes: [],
-			selectedCategories: [],
-			selectedSubCategories: [],
-			showCategories: false,
-			showTypes: false,
-		}
-	},
 	props: {
 		dateSpots: Object,
 		city: String,
 		types: Object,
 		categories: Object,
-		subCategories: Object,
+		subcategories: Object,
 	},
 	methods: {
-		saveAndCloseFilter() {
-			console.log('Selected Types:', this.selectedTypes);
-			console.log('Selected Categories:', this.selectedCategories);
-			// Add logic to save filter options if needed
-			// For now, just close the filter
+		saveAndCloseFilter(types, categories, subcategories) {
+			const filterData = {
+				selectedTypes: types,
+				selectedCategories: categories,
+				selectedSubcategories: subcategories,
+			};
+
+			router.post(`/date-spots/${this.$route.query.city}`, filterData)
 		},
+	},
+	updated() {
+		console.log(this.dateSpots)
+	},
+	computed: {
+		noDateSpots: function () {
+			return Object.keys(this.dateSpots).length === 0;
+		}
 	}
 
 }
@@ -43,15 +43,18 @@ export default {
 		<div class="md:container md:mx-auto md:flex gap-4">
 
 			<DateSpotCityFilter
+					:city="city"
 					:types="types"
 					:categories="categories"
-					:subCategories="subCategories"
-					@saveFilter="saveAndCloseFilter"
+					:subcategories="subcategories"
 			/>
 
 
 			<div class="w-full md:w-3/4 p-4 md:pl-0">
 				<div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2">
+					<div v-if="noDateSpots">
+						<p>Sorry, we don't have Date Spots in {{ city }} for that.. yet!</p>
+					</div>
 					<DateSpotCard
 							v-for="dateSpot in dateSpots"
 							:key="dateSpot.id"
