1a80a3bebca09848f3f99465b9868ddf40010efc
sonny <sonnyproeger@outlook.com>
2023-11-14 17:15:08 +0100

    overwrote method for profile picture

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,50 +12,59 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
-    use HasFactory;
-    use HasProfilePhoto;
-    use Notifiable;
-    use TwoFactorAuthenticatable;
+	use HasApiTokens;
+	use HasFactory;
+	use HasProfilePhoto;
+	use Notifiable;
+	use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-        'two_factor_recovery_codes',
-        'two_factor_secret',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+		'two_factor_recovery_codes',
+		'two_factor_secret',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
-    protected $appends = [
-        'profile_photo_url',
-    ];
+	/**
+	 * The accessors to append to the model's array form.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $appends = [
+		'profile_photo_url',
+	];
+
+	protected function defaultProfilePhotoUrl(): string
+	{
+		$name = trim(collect(explode(' ', $this->name))->map(function ($segment) {
+			return mb_substr($segment, 0, 1);
+		})->join(' '));
+
+		return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color=FFFFFF&background=B76E79';
+	}
 }
