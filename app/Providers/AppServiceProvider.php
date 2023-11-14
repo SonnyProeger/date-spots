72782d4ed207e43736529036f5e786e51fe327e4
sonny <sonnyproeger@outlook.com>
2023-11-14 12:11:27 +0100

    forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
 	public function boot(): void
 	{
 		//
+		if ($this->app->isProduction()) {
+			URL::forceScheme('https');
+		}
 	}
 }
