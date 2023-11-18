2f04124f13df1bff9f6d8b3b95749b40e8fa7869
sonny <sonnyproeger@outlook.com>
2023-11-18 13:44:14 +0100

    Fixed css nesting

--- resources/js/Pages/Admin/Shared/LoadingButton.vue
+++ resources/js/Pages/Admin/Shared/LoadingButton.vue
@@ -1,5 +1,6 @@
 <template>
-	<button :disabled="loading" class="flex items-center">
+	<button :disabled="loading"
+	        class="flex items-center px-6 py-3 rounded bg-roseGold text-white text-sm leading-4 font-bold whitespace-nowrap hover:bg-red-700 focus:bg-red-700">
 		<div v-if="loading" class="btn-spinner mr-2"/>
 		<slot/>
 	</button>
