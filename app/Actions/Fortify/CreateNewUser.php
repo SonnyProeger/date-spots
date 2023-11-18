838354c46015e08f2c5431c0123aa6f8a449c0ab
sonny <sonnyproeger@outlook.com>
2023-11-18 14:04:14 +0100

    when registering as user, role is always user

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,7 +22,6 @@ class CreateNewUser implements CreatesNewUsers
 			'name' => ['required', 'string', 'max:255'],
 			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
 			'password' => $this->passwordRules(),
-			'role_id' => ['required'],
 			'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
 		])->validate();
 
@@ -30,7 +29,7 @@ class CreateNewUser implements CreatesNewUsers
 			'name' => $input['name'],
 			'email' => $input['email'],
 			'password' => Hash::make($input['password']),
-			'role_id' => $input['role_id'],
+			'role_id' => 4,
 		]);
 	}
 }
