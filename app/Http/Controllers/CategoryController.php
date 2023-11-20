24b65a7c3de55befc032b38feca5b12504f8d072
sonny <sonnyproeger@outlook.com>
2023-11-20 21:45:34 +0100

    Added CRUD for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,29 +3,71 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Models\Type;
+use App\Traits\CrudOperationsTrait;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Validation\Rule;
+use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
+	use CrudOperationsTrait;
+
 	/**
 	 * Display a listing of the resource.
 	 */
 	public function index() {
-		//
+		$this->authorize('viewAny', Category::class);
+
+		$filters = Request::all('search', 'trashed');
+
+		$query = $this->commonIndexLogic(Category::class, $filters);
+		$categories = $query->paginate(10)
+			->withQueryString()
+			->through(function ($category) {
+				return [
+					'id' => $category->id,
+					'name' => $category->name,
+					'type' => $category->type,
+					'deleted_at' => $category->deleted_at,
+				];
+			});
+
+		return Inertia::render('Admin/Pages/Categories/Index', [
+			'categories' => $categories,
+			'filters' => $filters,
+		]);
 	}
 
 	/**
 	 * Show the form for creating a new resource.
 	 */
 	public function create() {
-		//
+		$this->authorize('create', Category::class);
+
+		return Inertia::render('Admin/Pages/Categories/Create', [
+			'types' => Type::all(),
+		]);
 	}
 
 	/**
 	 * Store a newly created resource in storage.
 	 */
 	public function store(Request $request) {
-		//
+		$this->authorize('viewAny', Category::class);
+
+		Request::validate([
+			'name' => ['required', 'max:50', Rule::unique('categories')],
+			'type_id' => ['required']
+		]);
+
+		Category::create([
+			'name' => Request::get('name'),
+			'type_id' => Request::get('type_id'),
+		]);
+
+		return Redirect::route('categories.index')->with('success', 'Category created.');
 	}
 
 	/**
@@ -38,21 +80,45 @@ class CategoryController extends Controller
 	/**
 	 * Show the form for editing the specified resource.
 	 */
-	public function edit(Category $category) {
-		//
+	public function edit(string $id) {
+		$category = Category::withTrashed()->find($id);
+		$this->authorize('update', $category);
+
+		return Inertia::render('Admin/Pages/Categories/Edit', [
+			'category' => $category,
+			'types' => Type::all(),
+		]);
 	}
 
 	/**
 	 * Update the specified resource in storage.
 	 */
 	public function update(Request $request, Category $category) {
-		//
+		$this->authorize('update', $category);
+
+		Request::validate([
+			'name' => ['required', 'max:50', Rule::unique('categories')],
+		]);
+
+		$category->update(Request::only('name'));
+
+		return Redirect::back()->with('success', 'Category updated.');
 	}
 
 	/**
 	 * Remove the specified resource from storage.
 	 */
 	public function destroy(Category $category) {
-		//
+		$this->authorize('delete', $category);
+
+		$category->delete();
+		return Redirect::route('categories.index')->with('success', "$category->name deleted.");
+
+	}
+
+	public function restore(Category $category) {
+		$this->authorize('restore', $category);
+		$category->restore();
+		return Redirect::back()->with('success', 'Category restored.');
 	}
 }
---
+++ resources/js/Pages/Admin/Pages/Categories/Edit.vue
@@ -0,0 +1,88 @@
+<template>
+	<div>
+		<Head :title="`${form.name}`"/>
+		<div class="flex justify-start mb-8 max-w-3xl">
+			<h1 class="text-3xl font-bold">
+				<Link class="text-indigo-400 hover:text-indigo-600" :href="route('categories.index')">Categories</Link>
+				<span class="text-indigo-400 font-medium">/</span>
+				{{ form.name }}
+			</h1>
+		</div>
+		<trashed-message v-if="category.deleted_at" class="mb-6" @restore="restore"> This category has been deleted.
+		</trashed-message>
+		<div class="max-w-3xl bg-white rounded-md shadow overflow-hidden">
+			<form @submit.prevent="update">
+				<div class="flex flex-wrap -mb-8 -mr-6 p-8">
+					<text-input v-model="form.name" :error="form.errors.name"
+					            class="pb-8 pr-6 w-full lg:w-1/2"
+					            label="Name"/>
+					<select-input v-model="form.type_id" :error="form.errors.type_id" class="pb-8 pr-6 w-full lg:w-1/2"
+					              label="Type">
+						<option v-for="type in types" :key="type.id" :value="type.id"
+						        :selected="type.id === category.type_id">
+							{{ type.name }}
+						</option>
+					</select-input>
+				</div>
+				<div class="flex items-center px-8 py-4 bg-gray-50 border-t border-gray-100">
+					<button v-if="!category.deleted_at" class="text-red-600 hover:underline" tabindex="-1" type="button"
+					        @click="destroy">Delete Category
+					</button>
+					<loading-button :loading="form.processing" class="btn-roseGold ml-auto" category="submit">Update Category
+					</loading-button>
+				</div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+import {Head, Link} from '@inertiajs/vue3'
+import TextInput from '@/Pages/Admin/Shared/TextInput.vue'
+import FileInput from '@/Pages/Admin/Shared/FileInput.vue'
+import SelectInput from '@/Pages/Admin/Shared/SelectInput.vue'
+import LoadingButton from '@/Pages/Admin/Shared/LoadingButton.vue'
+import TrashedMessage from '@/Pages/Admin/Shared/TrashedMessage.vue'
+import AdminAppLayout from "@/Pages/Admin/AdminAppLayout.vue";
+
+export default {
+	components: {
+		FileInput,
+		Head,
+		Link,
+		LoadingButton,
+		SelectInput,
+		TextInput,
+		TrashedMessage,
+	},
+	layout: AdminAppLayout,
+	props: {
+		category: Object,
+		types: Object,
+	},
+	remember: 'form',
+	data() {
+		return {
+			form: this.$inertia.form({
+				_method: 'put',
+				name: this.category.name,
+			}),
+		}
+	},
+	methods: {
+		update() {
+			this.form.post(`/admin/categories/${this.category.id}`)
+		},
+		destroy() {
+			if (confirm('Are you sure you want to delete this category?')) {
+				this.$inertia.delete(`/admin/categories/${this.category.id}`)
+			}
+		},
+		restore() {
+			if (confirm('Are you sure you want to restore this category?')) {
+				this.$inertia.put(`/admin/categories/${this.category.id}/restore`)
+			}
+		},
+	},
+}
+</script>
