2768ae63bf593b089e910228eee5ec7a4dcaf04f
sonny <sonnyproeger@outlook.com>
2023-11-20 21:44:42 +0100

    Formatting

--- app/Traits/CrudOperationsTrait.php
+++ app/Traits/CrudOperationsTrait.php
@@ -6,10 +6,9 @@ trait CrudOperationsTrait
 {
 	use FilterableTrait;
 
-	public function commonIndexLogic($model, $filters = []) {
+	public function commonIndexLogic($model, $filters) {
 
 		$query = $model::query()->orderBy('name');
-
 		return $this->applyFilters($query, $filters);
 	}
 
