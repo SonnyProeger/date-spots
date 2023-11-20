085fb150738e0db7035ca23f25609a1a7098a300
sonny <sonnyproeger@outlook.com>
2023-11-20 21:46:26 +0100

    Moved Dashboard to own directory

--- resources/js/Pages/Admin/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Admin/Pages/Dashboard/Index.vue
@@ -1,20 +1,20 @@
-<template>
-	<div>
-		<Head title="Dashboard"/>
-		<h1 class="mb-8 text-3xl font-bold">Dashboard</h1>
-		<p class="mb-8 leading-normal">Hey there! Welcome to Date Spots CRM, a demo app designed to help illustrate how <a
-				class="text-indigo-500 hover:text-orange-600 underline" href="https://inertiajs.com">Inertia.js</a> works.</p>
-	</div>
-</template>
-
 <script>
-import {Head} from '@inertiajs/vue3'
-import Layout from '@/Pages/Admin/Shared/Layout.vue'
+import AdminAppLayout from "@/Pages/Admin/AdminAppLayout.vue";
+import {Head} from "@inertiajs/vue3";
 
 export default {
-	components: {
-		Head,
-	},
-	layout: Layout,
+	name: "AdminDashboard",
+	components: {Head, AdminAppLayout},
 }
 </script>
+
+<template>
+	<AdminAppLayout title="Dashboard">
+		<h1 class="mb-8 text-3xl font-bold">Dashboard</h1>
+		<p class="mb-8 leading-normal">Hey there! Welcome to Date Spots Admin Dashboard </p>
+	</AdminAppLayout>
+</template>
+
+<style scoped>
+
+</style>
