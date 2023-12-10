72f079d0842b4f7ef6b8658d244d45525c31df98
sonny <sonnyproeger@outlook.com>
2023-12-10 08:32:39 +0100

    added function space replace

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -8,4 +8,9 @@ class StringHelper
 		// Use str_replace to replace hyphens with spaces
 		return str_replace('-', ' ', $string);
 	}
+
+	public static function replaceSpacesWithHyphens($string): array|string {
+		// Use str_replace to replace spaces with hyphens
+		return str_replace(' ', '-', $string);
+	}
 }
