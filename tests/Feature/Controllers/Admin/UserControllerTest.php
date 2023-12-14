b505614d8b8a4a7d72c06f72d3fc784e1759351e
sonny <sonnyproeger@outlook.com>
2023-12-14 15:43:33 +0100

    Added Tests

---
+++ tests/Feature/Controllers/Admin/UserControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Feature\Controllers\Admin;
+
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+	/**
+	 * A basic feature test example.
+	 */
+	public function test_example(): void {
+		$response = $this->get('/');
+
+		$response->assertStatus(200);
+	}
+}
