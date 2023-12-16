060d6c1ad7348126414032f03395333713c26401
sonny <sonnyproeger@outlook.com>
2023-12-16 04:16:15 +0100

    Refactored bloated Controller to use DatespotService class

--- app/Services/UserDatespotService.php
+++ app/Services/UserDatespotService.php
@@ -48,13 +48,13 @@ class UserDatespotService
 
 	public function getDatespotsByLocation($city) {
 		$datespots = Datespot::withCount('reviews')
-			->addSelect(DB::raw("(SELECT COUNT(*) FROM datespots) AS all_datespots"))
 			->withAvg('reviews', 'rating')
 			->with('media')
 			->get()
 			->where('city', $city)
 			->map(function ($datespot) {
 				$avgRating = round($datespot->reviews_avg_rating, 2);
+				$allDatespotsCount = Datespot::count(); // Count all records in YourModel (Datespot)
 				return [
 					'id' => $datespot->id,
 					'name' => $datespot->name,
@@ -63,11 +63,22 @@ class UserDatespotService
 					'photo_url' => $datespot->photo_url,
 					'rating' => $avgRating,
 					'reviews_count' => $datespot->reviews_count,
-					'all_datespots' => $datespot->all_datespots,
+					'all_datespots' => $allDatespotsCount,
 					'cover_image' => $datespot->getFirstTemporaryUrl(Carbon::now()->addMinutes(5), 'images'),
 				];
 			});
 		return $datespots;
+	}
+
+	public function getAllTypesWithCategoriesAndSubcategories(): Collection|array {
+		return Type::with([
+			'categories' => function ($query) {
+				$query->select('id', 'name', 'type_id');
+			},
+			'categories.subcategories' => function ($query) {
+				$query->select('id', 'name', 'category_id');
+			}
+		])->select('id', 'name')->get();
 	}
 
 	public function getSubcategoriesForDatespots($datespots): Collection {
@@ -96,10 +107,92 @@ class UserDatespotService
 		return $types;
 	}
 
-	public function filterDateSpotsByLocation($city, $requestData): Collection|array {
-		$query = Datespot::query()->where('city', $city);
+	public function filterDateSpotsByLocation($city, $request) {
+		$query = Datespot::query();
 
-		return $query->get();
+		if ($city) {
+			$query->where('city', $city);
+		}
+
+		$requestData = json_decode($request->getContent(), true);
+
+
+		if (!empty($requestData['selectedTypes']) || !empty($requestData['selectedCategories']) || !empty($requestData['selectedSubcategories'])) {
+			{
+				if ($request->filled('selectedTypes')) {
+					$selectedTypes = $request->input('selectedTypes');
+					$query->whereHas('types', function ($typeQuery) use ($selectedTypes) {
+						$typeQuery->whereIn('types.id', $selectedTypes);
+					});
+				} elseif ($request->filled('selectedCategories')) {
+					// If types are not selected but a category under type is selected
+					$selectedCategories = collect($request->input('selectedCategories'))
+						->map(function ($category) {
+							return explode('-', $category)[1];
+						})
+						->toArray();
+
+					$query->whereHas('categories', function ($categoryQuery) use ($selectedCategories) {
+						$categoryQuery->whereIn('categories.id', $selectedCategories);
+					});
+				} elseif ($request->filled('selectedSubcategories')) {
+					// If neither types nor categories are selected but a subcategory is selected
+					$selectedSubcategories = collect($request->input('selectedSubcategories'))
+						->map(function ($subcategory) {
+							return explode('-', $subcategory)[1];
+						})
+						->toArray();
+
+					$query->whereHas('subcategories', function ($subcategoryQuery) use ($selectedSubcategories) {
+						$subcategoryQuery->whereIn('subcategories.id', $selectedSubcategories);
+					});
+				}
+			}
+			$filteredDatespots = $query->with('types', 'categories', 'subcategories')
+				->withCount('reviews')
+				->withAvg('reviews', 'rating')
+				->with('media')
+				->get()
+				->where('city', $city)
+				->map(function ($datespot) {
+					$avgRating = round($datespot->reviews_avg_rating, 2);
+					$allDatespotsCount = Datespot::count();
+
+					return [
+						'id' => $datespot->id,
+						'name' => $datespot->name,
+						'city' => $datespot->city,
+						'tagline' => $datespot->tagline,
+						'photo_url' => $datespot->photo_url,
+						'rating' => $avgRating,
+						'reviews_count' => $datespot->reviews_count,
+						'all_datespots' => $allDatespotsCount,
+						'cover_image' => $datespot->getFirstMediaUrl('images'),
+					];
+				});
+		} else {
+			$filteredDatespots = $query->withCount('reviews')
+				->addSelect(DB::raw("(SELECT COUNT(*) FROM datespots) AS all_datespots"))
+				->withAvg('reviews', 'rating')
+				->with('media')
+				->get()
+				->where('city', $city)
+				->map(function ($datespot) {
+					$avgRating = round($datespot->reviews_avg_rating, 2);
+					return [
+						'id' => $datespot->id,
+						'name' => $datespot->name,
+						'city' => $datespot->city,
+						'tagline' => $datespot->tagline,
+						'photo_url' => $datespot->photo_url,
+						'rating' => $avgRating,
+						'reviews_count' => $datespot->reviews_count,
+						'all_datespots' => $datespot->all_datespots,
+						'cover_image' => $datespot->getFirstMediaUrl('images'),
+					];
+				});
+		}
+		return $filteredDatespots;
 	}
 
 	/**
