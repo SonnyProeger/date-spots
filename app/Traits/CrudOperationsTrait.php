9905b1ecec8fd680f8ca1878c924b4eec7053a78
sonny <sonnyproeger@outlook.com>
2023-12-16 03:50:38 +0100

    Removed redundant methods

--- app/Traits/CrudOperationsTrait.php
+++ app/Traits/CrudOperationsTrait.php
@@ -11,28 +11,4 @@ trait CrudOperationsTrait
 		$query = $model::query()->orderBy('name');
 		return $this->applyFilters($query, $filters);
 	}
-
-
-	public function create() {
-		// Logic for create method
-	}
-
-	public function store() {
-		// Logic for store method
-	}
-
-	public function edit() {
-		// Logic for edit method
-	}
-
-	public function update() {
-		// Logic for update method
-	}
-
-	public function destroy() {
-		// Logic for destroy method
-	}
-
-	// You can add more methods as needed...
-
 }
