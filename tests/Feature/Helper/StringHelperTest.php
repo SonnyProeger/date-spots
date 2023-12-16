f5c8fa1c91e95500d9defb38dec17fe6d8d9e3f9
sonny <sonnyproeger@outlook.com>
2023-12-16 03:56:40 +0100

    Added tests for Helper methods

---
+++ tests/Feature/Helper/StringHelperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Feature\Helper;
+
+use App\Helpers\StringHelper;
+use PHPUnit\Framework\TestCase;
+
+class StringHelperTest extends TestCase
+{
+	/** @test */
+	public function it_replaces_hyphens_with_spaces() {
+		$inputString = 'hello-world';
+		$expectedResult = 'hello world';
+
+		$result = StringHelper::replaceHyphensWithSpaces($inputString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	/** @test */
+	public function it_replaces_spaces_with_hyphens() {
+		$inputString = 'hello world';
+		$expectedResult = 'hello-world';
+
+		$result = StringHelper::replaceSpacesWithHyphens($inputString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+}
