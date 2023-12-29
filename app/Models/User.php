da5af73cd22e5d8ff1a0c634db0ec868cb434941
sonny <sonnyproeger@outlook.com>
2023-12-29 05:06:03 +0100

    Transactional E-mails implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Eloquent;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -70,7 +70,7 @@ use Laravel\Sanctum\PersonalAccessToken;
  * @method static Builder|User withoutTrashed()
  * @mixin Eloquent
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
 	use HasApiTokens;
 	use HasFactory;
