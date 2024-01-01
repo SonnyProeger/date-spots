824827e0d818b8ba12c2cb92590f33ddf9fb836a
sonny <sonnyproeger@outlook.com>
2024-01-02 00:23:46 +0100

    updated applayout

--- resources/js/Layouts/newAppLayout.vue
+++ resources/js/Layouts/newAppLayout.vue
@@ -24,7 +24,7 @@ export default {
 </script>
 
 <template>
-	<div id="app" class="min-h-screen bg-cream text-gray-800">
+	<div id="app" class="flex flex-col h-screen bg-cream text-gray-800">
 		<div class="container md:w-4/5 mx-auto">
 			<header class="md:mb-8 md:pt-4 md:py-0 py-4 px-2 flex justify-between items-center select-none">
 				<div class="text-2xl font-semibold flex justify-between items-center">
@@ -34,21 +34,21 @@ export default {
 				</div>
 
 				<div class="text-gray-800 sm:block md:hidden">
-					<div @click="isOpen =! isOpen" v-show="!isOpen">
+					<div v-show="!isOpen" @click="isOpen =! isOpen">
 						<svg
 								class="fill-current w-8 cursor-pointer"
-								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
 						>
 							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
 						</svg>
 					</div>
 
-					<div @click="isOpen =! isOpen" v-show="isOpen">
+					<div v-show="isOpen" @click="isOpen =! isOpen">
 						<svg
 								class="fill-current w-8 cursor-pointer"
-								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 20 20"
+								xmlns="http://www.w3.org/2000/svg"
 						>
 							<path
 									d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
@@ -63,7 +63,7 @@ export default {
 						Datespots
 					</Link>
 
-					<Link href="#" class="py-2 px-3 ml-2 hover:bg-rose-700 hover:text-white rounded">
+					<Link class="py-2 px-3 ml-2 hover:bg-rose-700 hover:text-white rounded" href="#">
 						Review
 					</Link>
 
@@ -81,24 +81,25 @@ export default {
 
 					</Link>
 
-					<Dropdown class="pl-8" v-else>
+					<Dropdown v-else class="pl-8">
 						<template #trigger>
 							<button v-if="$page.props.jetstream.managesProfilePhotos"
 							        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-rose-700 transition duration-300 ease-in-out transform hover:border-rose-700 hover:scale-105">
-								<img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_path"
-								     :alt="$page.props.auth.user.name">
+								<img :alt="$page.props.auth.user.name" :src="$page.props.auth.user.profile_photo_path"
+								     class="h-8 w-8 rounded-full object-cover">
 							</button>
 
 							<span v-else class="inline-flex rounded-md">
-                <button type="button"
-                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                <button
+		                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150"
+		                type="button">
                     {{ $page.props.auth.user.name }}
 
-                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                         fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                    <svg class="ms-2 -me-0.5 h-4 w-4" fill="none"
+                         stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.5 8.25l-7.5 7.5-7.5-7.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
                     </svg>
                 </button>
             </span>
@@ -135,8 +136,8 @@ export default {
 
 			<!-- Mobile Links -->
 			<div
-					class="bg-white px-4 pt-4 select-none border-b md:hidden"
 					v-if="isOpen"
+					class="bg-white px-4 pt-4 select-none border-b md:hidden"
 			>
 				<Link
 						:href="route('datespots')"
@@ -146,8 +147,8 @@ export default {
 				</Link>
 
 				<Link
-						href="#"
 						class="block mb-2 font-semibold text-white py-2 px-3 hover:bg-rose-700 bg-roseGold rounded cursor-pointer"
+						href="#"
 				>
 					Review
 				</Link>
@@ -167,15 +168,15 @@ export default {
 				</Link>
 			</div>
 		</div>
-		<main>
+		<main class="flex flex-1">
 			<slot/>
 		</main>
-
+		<footer class="flex justify-center py-4 text-gray-500 text-sm bg-cream">
+			&copy; 2023 Datespots. All rights reserved.
+		</footer>
 	</div>
 	<!-- Footer Section -->
-	<footer class="py-4 text-center text-gray-500 text-sm bg-cream">
-		&copy; 2023 Datespots. All rights reserved.
-	</footer>
+
 </template>
 
 <style scoped>
