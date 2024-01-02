1a58758f30aa803161952fea2a2681bdb5514e4e
sonny <sonnyproeger@outlook.com>
2024-01-02 07:04:27 +0100

    Removed a few unnecessary categories

--- database/factories/SubcategoryFactory.php
+++ database/factories/SubcategoryFactory.php
@@ -31,7 +31,7 @@ class SubcategoryFactory extends Factory
 			'movie theaters' => ['Blockbuster movies', 'Indie films'],
 			'live music' => ['Concerts', 'Live bands'],
 			'concerts' => ['Music festivals', 'Live performances'],
-			'art galleries' => ['Contemporary art', 'Local artists\' exhibitions'],
+			'art galleries' => ['Contemporary art', 'Local artists', 'exhibitions'],
 			'museums' => ['History museums', 'Science exhibits'],
 			'comedy clubs' => ['Stand-up comedy shows', 'Improv nights'],
 			'arcades' => ['Retro arcades', 'Modern gaming centers'],
@@ -41,10 +41,8 @@ class SubcategoryFactory extends Factory
 			'dance classes' => ['Dance lessons', 'Partner dance classes'],
 			'escape rooms' => ['Adventure escape rooms', 'Mystery puzzles'],
 			'anniversary' => ['Romantic dinner', 'Getaway'],
-			'birthday celebrations' => ['Party venues', 'Activity-based celebrations'],
 			'engagement locations' => ['Romantic locations', 'Scenic spots'],
-			'wedding venues' => ['Elegant venues', 'Outdoor settings'],
-			'family gatherings' => ['Family-friendly activities', 'Home celebrations'],
+			'hotel' => ['Luxury hotel', 'Boutique hotel', 'Romantic hotel'],
 		];
 
 		$randomCategory = Category::inRandomOrder()->first();
