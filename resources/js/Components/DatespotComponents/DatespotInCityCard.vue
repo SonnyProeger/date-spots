aacbfbd631785f714fc252c7e9a942aedd8fcc31
sonny <sonnyproeger@outlook.com>
2024-01-06 09:42:55 +0100

    Finished datespot in cities cards

---
+++ resources/js/Components/DatespotComponents/DatespotInCityCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+	name: "DatespotInCityCard",
+	props: {
+		city: Object,
+	},
+	methods: {
+		navigateToCity(cityName) {
+			this.$inertia.visit(route('user-datespot.filter-by-location', cityName));
+		},
+	},
+}
+</script>
+
+<template>
+	<div @click="navigateToCity(city.name)" class="group relative cursor-pointer">
+		<div
+				class="rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-50 hover:scale-105">
+			<img :src="city.imageSrc" :alt="city.altText"/>
+		</div>
+
+	</div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Components/DatespotComponents/DatespotsInCities.vue
+++ resources/js/Components/DatespotComponents/DatespotsInCities.vue
@@ -1,9 +1,41 @@
 <script>
-import StepCard from "@/Components/StepCard.vue";
+
+import DatespotInCityCard from "@/Components/DatespotComponents/DatespotInCityCard.vue";
 
 export default {
 	name: "DatespotsInCities",
-	components: {StepCard}
+	components: {DatespotInCityCard},
+	data() {
+		return {
+			cities: [
+				{
+					name: "Amsterdam",
+					imageSrc: "https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Amsterdam.png",
+					altText: "Amsterdam Image",
+				},
+				{
+					name: "Den Haag",
+					imageSrc: "https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Den+Haag.png",
+					altText: "Den Haag Image",
+				},
+				{
+					name: "Rotterdam",
+					imageSrc: "https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Rotterdam.png",
+					altText: "Rotterdam Image",
+				},
+				{
+					name: "Utrecht",
+					imageSrc: "https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Utrecht.png",
+					altText: "Utrecht Image",
+				},
+				// Add other cities
+			],
+		};
+	},
+	methods: {
+		goToAllCities() {
+		},
+	},
 }
 </script>
 
@@ -12,40 +44,10 @@ export default {
 		<div class="max-w-7xl mx-auto px-4 sm:px-0 md:container md:w-4/5">
 			<div class="flex justify-between">
 				<h2 class="text-lg md:text-3xl font-extrabold text-gray-900">Datespots in top destinations</h2>
-				<p class="text-lg  text-gray-900 ">See All</p>
+				<p @click="goToAllCities" class="text-lg text-gray-900 cursor-pointer">See All</p>
 			</div>
 			<div class="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-4">
-				<div class="group relative">
-					<div
-							class="rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-50 hover:scale-105 hover:cursor-pointer">
-						<img src="https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Amsterdam.png"
-						     alt="altText"/>
-					</div>
-				</div>
-
-				<div class="group relative">
-					<div
-							class="rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-50 hover:scale-105 hover:cursor-pointer">
-						<img src="https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Den+Haag.png"
-						     alt="altText"/>
-					</div>
-				</div>
-
-				<div class="group relative">
-					<div
-							class="rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-50 hover:scale-105 hover:cursor-pointer">
-						<img src="https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Rotterdam.png"
-						     alt="altText"/>
-					</div>
-				</div>
-
-				<div class="group relative">
-					<div
-							class="rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-50 hover:scale-105 hover:cursor-pointer">
-						<img src="https://datespotsbucket.s3.eu-central-1.amazonaws.com/media/DatespotCities/Utrecht.png"
-						     alt="altText"/>
-					</div>
-				</div>
+				<DatespotInCityCard v-for="city in cities" :key="city.name" :city="city"/>
 			</div>
 		</div>
 	</div>
