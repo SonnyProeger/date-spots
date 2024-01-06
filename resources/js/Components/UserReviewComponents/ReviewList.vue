583241cc4a63a80d448fe1e0da8097a8c565446a
sonny <sonnyproeger@outlook.com>
2024-01-06 11:50:21 +0100

    added failsafe if not logged in or no reviews

--- resources/js/Components/UserReviewComponents/ReviewList.vue
+++ resources/js/Components/UserReviewComponents/ReviewList.vue
@@ -14,12 +14,18 @@ export default {
 <template>
 	<div class="bg-white">
 		<div class="container mx-auto md:w-4/5">
-
 			<div class="w-2/3 flex flex-col space-y-6 py-6">
 				<h1 class="font-extrabold text-2xl"> Your Reviews</h1>
-				<div v-for="review in this.reviews.data">
-					<UserReviewItem :review="review"/>
+
+				<div v-if="this.reviews.data">
+					<div v-for="review in this.reviews.data">
+						<UserReviewItem :review="review"/>
+					</div>
 				</div>
+				<div v-else>
+					<h1> You have no reviews yet. After you write some reviews, they will appear here.</h1>
+				</div>
+
 			</div>
 		</div>
 	</div>
