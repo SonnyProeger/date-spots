cc8397397139b8b0e6fb8960a454f7ecd62aec17
sonny <sonnyproeger@outlook.com>
2024-01-06 12:01:18 +0100

    Added svg to search bar

--- resources/js/Components/UserReviewComponents/ReviewSearchBar.vue
+++ resources/js/Components/UserReviewComponents/ReviewSearchBar.vue
@@ -52,14 +52,29 @@ export default {
 
 <template>
 	<div class="relative w-full">
+		<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 z-10"
+		>
+			<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+			/>
+		</svg>
 		<input
-				class="w-full text-center md:text-left px-4 py-2 rounded-lg border-gray-300 focus:border-roseGold focus:ring-darkRoseGold rounded-md shadow-sm"
+				class="w-full text-center md:text-left pl-10 md:pl-12 py-2 rounded-lg border-gray-300 focus:border-roseGold focus:ring-darkRoseGold rounded-md shadow-sm z-0"
 				v-model="searchText"
 				@input="fetchAutocompleteResults"
 				id="ReviewSearchBar"
 				type="text"
 				placeholder="What would you like to review?"
 		/>
+
 		<ul
 				v-if="showSuggestions"
 				class="absolute left-0 mt-2 w-full bg-white border rounded-md shadow-md z-10"
