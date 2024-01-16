20b9268fbb31a15646bad3da7d43f6e7e3a08bc2
sonny <sonnyproeger@outlook.com>
2024-01-16 11:52:32 +0100

    changed seeder for consistency

--- database/seeders/TypesTableSeeder.php
+++ database/seeders/TypesTableSeeder.php
@@ -14,7 +14,14 @@ class TypesTableSeeder extends Seeder
 	public function run(): void {
 		$datespots = Datespot::all();
 
-		$types = Type::factory(5)->create();
+		// Create all 5 types
+		$types = collect([
+			Type::create(['name' => 'Activities']),
+			Type::create(['name' => 'Entertainment']),
+			Type::create(['name' => 'Food']),
+			Type::create(['name' => 'Outdoor']),
+			Type::create(['name' => 'Special Occasions']),
+		]);
 
 		$datespots->each(function ($datespot) use ($types) {
 			$typeIds = $types->random(rand(1, 3))->pluck('id')->toArray();
